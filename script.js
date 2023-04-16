980f9c8757c3267aa5ea8f3759693629f6c8181c
GiorgiaModi <87370065+GiorgiaModi@users.noreply.github.com>
2023-04-16 16:29:12 +0200

    change of some components

--- script.js
+++ script.js
@@ -105,10 +105,7 @@ function initSwiper() {
 
         slidesPerView: 1,
         spaceBetween: 10,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
+
         breakpoints: {
             640: {
                 slidesPerView: 2,
